17224fad57b9a235a01ac515e24a77b51c9a387a
gianlucafalaschi <gian100gian@outlook.it>
2024-10-25 19:51:34 +0200

    add: card conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,29 @@ function App() {
     <>
       {/* <Navbar></Navbar> */}
       <div className='grid grid-cols-4 gap-5'>
-        <Card title="Tokyo" imgUrl="https://images.unsplash.com/photo-1633632860411-364d4dc29309?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+        <Card
+          isVisited={true} 
+          title="Tokyo" 
+          imgUrl="https://images.unsplash.com/photo-1633632860411-364d4dc29309?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. A, animi quidem.
         </Card>
-        <Card title="New York" imgUrl="https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat recusandae non deleniti harum. Molestias?
+        <Card
+          isVisited={false} 
+          title="New York" 
+          imgUrl="https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat recusandae non deleniti harum. Molestias?
         </Card>
-        <Card title="Rome" imgUrl="https://images.unsplash.com/photo-1529154036614-a60975f5c760?q=80&w=876&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, est.
+        <Card 
+          isVisited={true}
+          title="Rome" 
+          imgUrl="https://images.unsplash.com/photo-1529154036614-a60975f5c760?q=80&w=876&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, est.
         </Card>
-        <Card title="Paris" imgUrl="https://images.unsplash.com/photo-1502602898657-3e91760cbb34?q=80&w=873&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        <Card
+          isVisited={false} 
+          title="Paris" 
+          imgUrl="https://images.unsplash.com/photo-1502602898657-3e91760cbb34?q=80&w=873&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
         </Card>
       </div>
       {/* <div>
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,7 +7,7 @@ function Card(props) {
 */
 
 /* A cleaner way to pass the Props */ /* Note: with "children" I only pass the props inside <Card>I pass this</Card>, in this case the text lorem ipsum  */
-function Card({title, imgUrl, children}) {
+function Card({title, imgUrl, isVisited, children}) {
     
     return(
         <div className="rounded-md overflow-hidden bg-zinc-950">
@@ -17,6 +17,12 @@ function Card({title, imgUrl, children}) {
                 <p className="text-gray-500">
                     {children}
                 </p>
+                {/* CONDITIONAL RENDERING */}
+                {/* ternary operator, show whether the city is visited or not */}
+                {/* {isVisited ?<span><i className="fa-solid fa-check"></i> visited</span> : <span><i className="fa-solid fa-xmark"></i> not visited</span> } */}
+                {/* another option for conditional rendering instead of ternary operator */}
+                {isVisited && <span><i className="fa-solid fa-check"></i> visited</span>}
+                {!isVisited && <span><i className="fa-solid fa-xmark"></i> not visited</span>}
             </div>
         </div>
     )
