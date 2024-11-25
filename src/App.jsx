d01160f832c167c80d6d45b21b2b0b8d7a5fee23
gianlucafalaschi <gian100gian@outlook.it>
2024-11-25 21:13:26 +0100

    add: comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState, useEffect, useReducer, useContext } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 import Card from './components/Card'
@@ -8,11 +5,13 @@ import CardForm from './components/CardForm'
 import TestTailwind from './components/TestTailwind'
 import Example from './components/Example'
 import { ProvaContext } from './stores/ProvaContext'
+// Importing the useSelector hook from React Redux to access the Redux store state
 import { useSelector } from 'react-redux'
 
 
 
 function App() {
+  // Accessing the cities state from the Redux store
   const cities = useSelector((state) => state.cities.value);
 
   return (
@@ -21,8 +20,9 @@ function App() {
       <CardForm></CardForm>
       {/* <Navbar></Navbar> */}
       <div className='grid grid-cols-4 gap-5 mb-3'>        
-        {/* LIST RENDERING - with array.maps I scroll all the 
-        elements in the array -  Remember to add the key id  when use the map  */ }
+        {/* LIST RENDERING - Using array.map to iterate over 
+        each city and render a Card component for each 
+        -  Remember to add the key id  when use the map  */ }
         {cities.map((city) => (
           <Card
             key={city.id}
