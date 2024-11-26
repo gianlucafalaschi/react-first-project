ab00d6a8b2495b4601e80e6ec9e81a9ec96aaf5c
gianlucafalaschi <gian100gian@outlook.it>
2024-11-26 18:23:41 +0100

    add: increment and decrement with counterSlice

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -1,59 +1,26 @@
-/* HOW TO USE useEffect
-An Effect lets you keep your component synchronized with some 
-external system (
+import { increment, decrement } from "../redux/counterSlice";
+import { useSelector, useDispatch } from "react-redux";
 
-import { useEffect } from 'react';
-
-useEffect(() => {
-  // Effetto collaterale qui
-}, [dipendenze]);
-
-Nota: Array delle dipendenze:
-[], [dipendenze]
-[]: Se lasci l'array delle dipendenze vuoto, l'effetto viene eseguito solo una volta dopo il montaggio iniziale del componente.
-
-[dipendenze]: L'effetto viene eseguito ogni volta che una delle dipendenze cambia.
-
-
-*/
-
-import { useState } from "react";
-import { useEffect } from "react";
-import { useContext } from "react";
-import { ProvaContext } from "../stores/ProvaContext";
-
-function Example(cities) {
-    //const [count, setCount] = useState(0);
-    const {count, setCount} = useContext(ProvaContext);
-    /* By doing this the count and the title are not synchronized, I need 
-    to use useEffect  */
-    /* 
-        function handleClick() {
-            setCount(count + 1);
-            document.title = `Conteggio: ${count}`;
-        };
-     */
-
-
-
-    /* With useEffect */
-
-   /*  function handleClick() {
-        setCount(count + 1);
-    };
-
-
-    useEffect(() => {
-        document.title = `Conteggio: ${count}`;
-        console.log('Ciao da useEffect');
-    }, [count]); */
-
+function Example() {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+ 
     return (
-        <div>
-            <p>Conteggio: {count}</p>
-            <button onClick={()=> setCount(count + 1)}>Increase</button>
+        <div className="mb-6">
+            <p className="mb3">Conteggio: {count}</p>
+            <button 
+                className="mr-4" 
+                aria-label="Increment value" 
+                onClick={() => dispatch(increment())}>
+                Incrementa +
+            </button>
+            <button
+                aria-label="Decrement value"
+                onClick={() => dispatch(decrement())}>
+                Decrementa -
+            </button>
         </div>
-    )
+    );
 }
 
-export default Example
+export default Example;
