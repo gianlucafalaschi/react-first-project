bd631cb3f1afc561e394fce728813273575628ce
gianlucafalaschi <gian100gian@outlook.it>
2024-12-08 16:34:22 +0100

    add: cards children page

---
+++ src/assets/pages/CardsChildren.jsx
@@ -0,0 +1,13 @@
+import NewNavbar from "../../components/NewNavbar";
+
+function CardsChildren(){
+    return(
+        <>
+            <NewNavbar></NewNavbar>
+            <h1 className="text-3xl font-bold mb-3">Cards Children</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero temporibus nihil illo esse at? Nam sunt voluptate tempora quo molestiae obcaecati maiores earum sit quibusdam doloremque cum reiciendis ex repudiandae, est a vel, repellat totam veritatis asperiores optio at enim! Et alias illum dignissimos error! Deserunt beatae inventore non doloribus assumenda delectus vero nam, harum eos corporis eaque tempora consectetur facere, aperiam similique nihil error explicabo est obcaecati. Consectetur, facilis voluptate. Est consectetur doloremque minima dignissimos ipsum ducimus aliquam in harum facilis, excepturi itaque eaque ea? Velit cum vitae suscipit unde magni, dolores quo iusto corrupti vero dolorum expedita repellendus.</p>
+        </>
+    );
+}
+
+export default CardsChildren
